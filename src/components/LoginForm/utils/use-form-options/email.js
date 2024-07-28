df77d4da3fd210459eb5bfcf2db11817db41d67f
Jhon (Garrux) <igarrux@gmail.com>
2024-07-28 02:40:47 -0500

    feat(useForm): :sparkles: Se crean archivos para las opciones de cada campo de texto

---
+++ src/components/LoginForm/utils/use-form-options/email.js
@@ -0,0 +1,14 @@
+import { EMAIL_REGEX } from '@src/utils/regex/regex';
+import { UI_TEXT } from '../../ui/text';
+
+export const EMAIL_OPTIONS = {
+	required: {
+		value: true,
+		message: UI_TEXT.EMAIL_REQUIRED,
+	},
+	pattern: {
+		value: EMAIL_REGEX,
+		message: UI_TEXT.EMAIL_IS_INVALID,
+	},
+};
+
