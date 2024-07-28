e9b9a5714cbccf0e98d5facc922c9a5123aedcaa
Jhon (Garrux) <igarrux@gmail.com>
2024-07-28 00:35:15 -0500

    fix(components/react/text-field): :bug: No debía tener un value ni onChange definido
    Dado que se espera que se comporte como un input, este componente no debia tener internamente un value, setValue ni onchange, por ende se removieron

--- src/components/react/text-field/text-field.jsx
+++ src/components/react/text-field/text-field.jsx
@@ -1,41 +1,44 @@
-import { useState } from 'react';
+import { forwardRef, useState } from 'react';
 import { EyeIcon } from './icons/eye.icon';
 import './text-field.style.css';
-export const TextField = ({
-	label,
-	supportText,
-	isValid,
-	supportIcon,
-	isShowPassword,
-	setIsShowPassword,
-	...props
-}) => {
-	const [value, setValue] = useState('');
-	if (props.type == 'email') props.type = 'text';
-	return (
-		<div className="text-field">
-			<label is-valid={`${isValid}`}>
-				<span>{label}</span>
-				{supportIcon && <div>{supportIcon}</div>}
-				<input
-					{...props}
-					value={value}
-					type={isShowPassword ? 'text' : props.type}
-					onChange={(e) => setValue(e.target.value)}
-					required
-				/>
-				{props?.type == 'password' && (
-					<div>
-						<EyeIcon
-							setIsOpen={setIsShowPassword}
-							isOpen={isShowPassword}
-						/>
-					</div>
-				)}
-			</label>
-			<span is-valid={`${isValid}`} className="support-text">
-				{supportText}
-			</span>
-		</div>
-	);
-};
+export const TextField = forwardRef(
+	(
+		{
+			label,
+			supportText,
+			isValid,
+			supportIcon,
+			isShowPassword,
+			setIsShowPassword,
+			...props
+		},
+		ref
+	) => {
+		if (props.type == 'email') props.type = 'text';
+		return (
+			<div className="text-field">
+				<label is-valid={`${isValid}`}>
+					<span>{label}</span>
+					{supportIcon && <div>{supportIcon}</div>}
+					<input
+						{...props}
+						ref={ref}
+						type={isShowPassword ? 'text' : props.type}
+						required
+					/>
+					{props?.type == 'password' && (
+						<div>
+							<EyeIcon
+								setIsOpen={setIsShowPassword}
+								isOpen={isShowPassword}
+							/>
+						</div>
+					)}
+				</label>
+				<span is-valid={`${isValid}`} className="support-text">
+					{supportText}
+				</span>
+			</div>
+		);
+	}
+);
