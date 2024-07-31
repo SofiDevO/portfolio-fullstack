a2ed07e10e91dfa6cd93cd0527091a555d4b2946
Jhon (Garrux) <igarrux@gmail.com>
2024-07-31 17:19:28 -0500

    feat(httpError): :sparkles: Ahora el manejador de errores http redirige al inicio cuando la respuesta es 401

--- src/services/httpError/httpError.js
+++ src/services/httpError/httpError.js
@@ -1,7 +1,10 @@
+import { PATHS } from '@src/constants/paths';
+
 export class HTTPError extends Error {
 	constructor(response) {
 		super(`HTTP Error: ${response.status}`);
 		this.status = response.status;
 		this.response = response;
+		if (response.status == 401) location.href = PATHS.INDEX;
 	}
 }
