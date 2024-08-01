45e939e1c71bd4f5c85dc7b292b43245055ccee6
Jhon (Garrux) <igarrux@gmail.com>
2024-07-31 22:33:05 -0500

    feat(reac/components): :sparkles: Se agrega toasts al dashboardProjects

--- src/react/components/BashboardProjects/DashboardProjects.jsx
+++ src/react/components/BashboardProjects/DashboardProjects.jsx
@@ -3,10 +3,13 @@ import PortfolioCard from '@src/react/organisms/PortfolioCard/PortfolioCard';
 import { UI_TEXT } from './ui/text';
 import { useEffect, useState } from 'react';
 import { getUserCards } from '@src/services/get-user-cards.service';
+import { toast } from 'react-toastify';
+import { getCardsToastParam } from './configs/get-cards-toast';
 export const DashboardProjects = () => {
 	const [userCards, setUserCards] = useState(null);
 	useEffect(() => {
-		getUserCards()
+		toast
+			.promise(getUserCards(), getCardsToastParam)
 			.then((res) => res.json())
 			.then(setUserCards);
 	}, []);
--- src/react/components/BashboardProjects/ui/text.js
+++ src/react/components/BashboardProjects/ui/text.js
@@ -1,3 +1,8 @@
 export const UI_TEXT = {
 	TITLE: 'Tarjetas de proyectos',
+	GET_CARDS_CONEXION_ERROR:
+		'No se pudieron obtener las tarjetas. Verifica tu conexión',
+	SERVER_ERROR: 'Hubo un problema inetrno al intentar obtener tus tarjetas',
+	GETTING_CARDS: 'Obteniendo tarjetas',
+	CARDS_RECEIVED: 'Tarjetas listas',
 };
