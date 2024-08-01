4fffb1f2b1f1d3a61c93068fd1a91653e2f68d49
Jhon (Garrux) <igarrux@gmail.com>
2024-07-31 22:13:30 -0500

    fix(pages/dashboard): :bug: Astro no puede agregar directivas al componente pasado en props, se opta por validación

--- src/pages/dashboard/config/static-paths.js
+++ src/pages/dashboard/config/static-paths.js
@@ -4,15 +4,13 @@ export const StaticPaths = [
 	{
 		params: { path: 'projects' },
 		props: {
-			Content: () => 'Projects' /**Component */,
 			route: PATHS.DASHBOARD_PROJECTS,
 		},
 	},
 	{
 		params: { path: undefined },
 		props: {
-			Content: () => 'index' /**Component */,
-			route: PATHS.DASHBOARD,
+			route: PATHS.DASHBOARD_PROJECTS,
 		},
 	},
 ];
